7c1c05b787eb80dd5bf99d5b65261b1ed64c5603
Brent Lemieux <brent.lemieux@ripeinsights.com>
2019-06-10 05:54:13 -0600

    update API testing

--- testapi.py
+++ testapi.py
@@ -39,13 +39,5 @@ if __name__ == '__main__':
     dist_dict = get_feature_dists()
     ### Run locally outside of docker
     url = 'http://0.0.0.0:5000/api'
-    ### Connect to docker container docker run -p 8000:5000 blemi/model_api
-    # url = 'http://0.0.0.0:8000/api'
-    ### Connect to api on AWS
-    # url = 'http://ApiDemo-env-1.ge3hik39wt.us-west-2.elasticbeanstalk.com/api'
     for _ in range(1):
         resp = test_api(FEATURES, dist_dict, url=url)
-
-
-    # CURL TEST
-    # curl -X GET 'http://0.0.0.0:5000/api' --data {'INDUS': 5.9811172572674245, 'RM': 4.714079929999944, 'AGE': 80.57613195977545, 'DIS': 3.7703834318716734, 'NOX': 0.6968281505648362, 'PTRATIO': 13.629968799895778}
